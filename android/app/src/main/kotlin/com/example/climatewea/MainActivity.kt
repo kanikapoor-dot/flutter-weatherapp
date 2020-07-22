771cdc5c7bc36bcde4198fee5c30ac7bc9d5d212
kani kapoor <kanikapoor24@gmail.com>
2020-07-22 18:41:05 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/climatewea/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.climatewea
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:climatewea/services/location.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async{
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.longitude);
+    print(location.latitude);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+
+          },
+          child: Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
